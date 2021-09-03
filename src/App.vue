a73645675539129dc0b2243fb0cb50cf0caa6b1c
wangpeiyuan638 <wpy940916>
2021-09-03 15:28:18 +0800

    测试新建分支

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
+<!--
+ * @Descripttion:
+ * @version:
+ * @Author: sueRimn
+ * @Date: 2021-09-03 14:10:04
+ * @LastEditors: sueRimn
+ * @LastEditTime: 2021-09-03 15:27:18
+-->
 <template>
   <div id="app">
+    测试代码
     <router-view />
   </div>
 </template>
